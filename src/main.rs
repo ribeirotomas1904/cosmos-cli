7860e751963623871262abd5066f2b24a97b0be1
Nathanael Ribeiro <ribeirotomas1904@gmail.com>
2024-06-28 08:57:49 -0300

    Improve usage of clap

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,24 @@
-use std::str::FromStr;
-
 use anyhow::Context;
 use clap::Parser;
 use cosmos::{Address, AddressHrp, CosmosNetwork, ParsedCoin, SeedPhrase};
 
 #[derive(Parser)]
 struct Args {
-    coin: String,
-    address: String,
+    coin: ParsedCoin,
+    address: Address,
 
     #[arg(long = "cosmos-wallet", env = "COSMOS_WALLET")]
-    cosmos_wallet: String,
+    cosmos_wallet: SeedPhrase,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    let parsed_coin = ParsedCoin::from_str(&args.coin)
-        .with_context(|| format!("Invalid coin: {:?}", args.coin))?;
+    let coins = vec![args.coin.into()];
 
-    let coins = vec![parsed_coin.into()];
-
-    let address = Address::from_str(&args.address)
-        .with_context(|| format!("Invalid address: {:?}", args.address))?;
-
-    let wallet = SeedPhrase::from_str(&args.cosmos_wallet)
-        .with_context(|| format!("Invalid cosmos wallet: {:?}", args.cosmos_wallet))?
+    let wallet = args
+        .cosmos_wallet
         .with_hrp(AddressHrp::from_static("osmo"))
         .with_context(|| "Invalid address HRP".to_string())?;
 
@@ -36,7 +28,7 @@ async fn main() -> anyhow::Result<()> {
         .with_context(|| "Failed to connect to the Osmosis testnet".to_string())?;
 
     let result = wallet
-        .send_coins(&cosmos, address, coins)
+        .send_coins(&cosmos, args.address, coins)
         .await
         .with_context(|| "Failed to send coins".to_string())?;
 
